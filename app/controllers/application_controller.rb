57c3eea04537cedef81db207236fd1ebe54d0db9
Alex Fiennes <alexfiennes246@gmail.com>
2018-08-28 18:50:48 +0100

    Changes to home and done design for forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+
+  def configure_permitted_parameters
+      # For additional fields in app/views/devise/registrations/new.html.erb
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+
+      # # For additional in app/views/devise/registrations/edit.html.erb
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
 end
