312aa6745afba63c5334b18177729aae64f842b9
Klaudiusz Zembrzuski <moa21@o2.pl>
2018-08-28 13:27:30 +0100

    Schema updated for first adn last manes for user and politicians

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def index
+    if params[:query].present?
+      sql_query = " \
+        user.title @@ :query \
+        OR movies.syllabus @@ :query \
+        OR directors.first_name @@ :query \
+        OR directors.last_name @@ :query \
+      "
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_163636) do
+ActiveRecord::Schema.define(version: 2018_08_28_121913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,10 +49,11 @@ ActiveRecord::Schema.define(version: 2018_08_27_163636) do
   end
 
   create_table "politicians", force: :cascade do |t|
-    t.string "name"
     t.string "party"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
   create_table "users", force: :cascade do |t|
@@ -63,6 +64,8 @@ ActiveRecord::Schema.define(version: 2018_08_27_163636) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
