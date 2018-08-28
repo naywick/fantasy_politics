58d537a30cf260dab37ffbff2c3e734d0f3a1eff
Nayela Wickramasuriya <nayelawick@gmail.com>
2018-08-28 18:24:48 +0100

    Controllers complete, some docs added for the political_party view with Jess's assistance

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,2 +1,28 @@
 class LeaguesController < ApplicationController
+  def index
+    @leagues = League.all
+  end
+
+  def create
+    @league = League.new(league_params)
+  end
+
+  def update
+    @league = League.find(params[:id])
+    if @league.update(league_params)
+      redirect_to league(@league)
+    else
+      render :edit
+  end
+
+  def destroy
+    @league = League.find(params[:id])
+    @league.destroy
+  end
+
+  private
+
+  def league_params
+    params.require(:league).permit(:name)
+  end
 end
--- app/controllers/political_party_controller.rb
+++ app/controllers/political_party_controller.rb
@@ -1,2 +1,38 @@
 class PoliticalPartyController < ApplicationController
+  def index
+    @political_parties = PoliticalParty.all
+  end
+
+  def new
+    @political_party = PoliticalParty.new
+  end
+
+  def create
+    @political_party = PoliticalParty.new(political_party_params)
+    @political_party.user = current_user
+    authorize = @political_party
+    if @political_party.save
+      redirect_to political_party(@political_party)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @political_party = PoliticalParty.find(params[:id])
+      if @political_party.update(political_party_params)
+      redirect_to political_party(@political_party)
+    else
+      render :edit
+  end
+
+  def destroy
+    @political_party.destroy
+  end
+
+  private
+
+  def political_party_params
+    params.require(:political_party).permit(:name)
+  end
 end
