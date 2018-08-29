872869ab06bcbca9f858693195021137b948ccfe
Klaudiusz Zembrzuski <moa21@o2.pl>
2018-08-29 15:15:45 +0100

    Navbar sorted

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,12 +1,12 @@
 class League < ApplicationRecord
   belongs_to :user
-  
+
   has_many :league_connections
   has_many :users, through: :league_connections
 
   def creator
     return self.user
-
+  end
 
   include AlgoliaSearch
 
