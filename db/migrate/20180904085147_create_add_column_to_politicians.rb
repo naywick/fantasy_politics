8c2591d1946fd8eaf8d4ec485a768455bab1c8c8
Alex Fiennes <alexfiennes246@gmail.com>
2018-09-04 16:03:52 +0100

    Starting league pages styling

--- db/migrate/20180904085147_create_add_column_to_politicians.rb
+++
@@ -1,9 +0,0 @@
-class CreateAddColumnToPoliticians < ActiveRecord::Migration[5.2]
-  def change
-    create_table :add_column_to_politicians do |t|
-      t.string :rank
-
-      t.timestamps
-    end
-  end
-end
